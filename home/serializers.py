eea3a93a9815c8a15b489327403ac2c2e30f3ef3
swastik <swastkk@gmail.com>
2022-12-28 19:24:28 +0530

    converted-into-zip

--- home/serializers.py
+++ home/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import *
+import shutil
 class FileListSerializer(serializers.Serializer):
     files= serializers.ListField(
         child= serializers.FileField(max_length= 1000000, allow_empty_file= False, use_url= False)
 
     )
     folder= serializers.CharField(required= False)
-    def zip_files():
-        pass
+
+    def zip_files(self, folder):
+        shutil.make_archive(str(folder), 'zip', f"media/{folder}")
+
     def create(self, validated_data):
                 folder= Folder.objects.create()
                 files= validated_data.pop('files')
@@ -15,5 +18,7 @@ class FileListSerializer(serializers.Serializer):
                 for file in files:
                   files_obj= Files.objects.create(folder= folder, file= file)
                   files_objs.append(files_obj) 
-        
+
+                self.zip_files(folder.uid)
+
                 return {'files': {}, 'folder': str(folder.uid)}
